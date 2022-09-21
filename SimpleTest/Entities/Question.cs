e1eff4251dcad5b510b1100180125a68881fd619
GoreC1 <hazich322@gmail.com>
2022-09-21 15:20:40 +0500

    First interfaces and entities

---
+++ SimpleTest/Entities/Question.cs
@@ -0,0 +1,13 @@
+﻿using SimpleTest.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleTest.Entities
+{
+    public class Question : IQuestion
+    {
+        public string Title { get; set; }
+    }
+}
---
+++ SimpleTest/Interfaces/IQuestion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleTest.Interfaces
+{
+    public interface IQuestion
+    {
+        string Title { get; set; }
+
+    }
+}
